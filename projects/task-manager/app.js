313777a0a3040968ccef1e9644be45e5b6b9b45a
mehrad <mehradvs4@gmail.com>
2026-08-03 21:32:09 +0330

    create UIManager class,add event listener

--- projects/task-manager/app.js
+++ projects/task-manager/app.js
@@ -48,3 +48,73 @@ class TaskManager{
         return JSON.parse(localStorage.getItem("tasks"))
     }
 }
+class UIManager {
+    constructor() {
+        this.manager = new TaskManager()
+    }
+    render() {
+        const todo = document.querySelector(".todo-column-container")
+        const progress = document.querySelector(".in-progress-column-container")
+        const done = document.querySelector(".done-column-container")
+        todo.innerHTML = ""
+        progress.innerHTML = ""
+        done.innerHTML = ""
+        for(let task of this.manager.tasks) {
+            const container = document.createElement("div")
+            const title = document.createElement("p")
+            const delBtn = document.createElement("button")
+            delBtn.textContent = "Delete"
+            delBtn.dataset.id = task.id
+            const priority = document.createElement("p")
+            const date = document.createElement("p")
+            title.textContent = `Title : ${task.title}`
+            priority.textContent = `Priority : ${task.priority}`
+            date.textContent = `Created At : ${task.createdAt.toDateString()}`
+            container.appendChild(title)
+            container.appendChild(priority)
+            container.appendChild(date)
+            container.appendChild(delBtn)
+            if(task.status === "todo") {
+                todo.appendChild(container)
+            }
+            else if(task.status === "progress") {
+                progress.appendChild(container)
+            }
+            else if(task.status === "done") {
+                
+                done.appendChild(container)
+            }
+        }
+    }
+    createTask() {
+        const title = document.getElementById("title").value.trim()
+        if(title.length===0) {
+            alert("title is empty")
+            return false
+        }
+        const status = document.getElementById("stat").value
+        if(status==="status") {
+            alert("choose status")
+            return false
+        }
+        const priority = document.getElementById("pri").value
+        if(priority==="priority") {
+            alert("choose priority")
+            return false
+        }
+        this.manager.addTask(title,status,priority)
+        this.render()
+        return true
+    }
+}
+document.addEventListener("DOMContentLoaded",() => {
+    const view = new UIManager() 
+    view.render()
+    document.querySelector("form").addEventListener("submit",(event) => {
+        event.preventDefault()
+        const isCreated = view.createTask()
+        if(isCreated) {
+            event.currentTarget.reset()
+        }
+    })
+})
